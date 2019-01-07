ff5a8e509d1478d49f593ea46c23903b423e72de
seglianikana <44862139+seglianikana@users.noreply.github.com>
2019-01-07 23:35:05 +0200

    Add files via upload
    домашня робота

---
+++ km84/Seglaynik_Anastasia/workshop1/source/inputter1.py
@@ -0,0 +1,5 @@
+a=2
+b=3
+def function1():
+    return (a + 4*b) (a-3*b) + 2*a
+    print("результат дорів")
--- km84/Seglaynik_Anastasia/workshop1/source/main.py
+++ km84/Seglaynik_Anastasia/workshop1/source/main.py
@@ -1,8 +1,8 @@
 """
-Тут написати умову до завдання
+1.	Розрахуйте значення виразу (a + 4b) (a-3b) + a2 при a = 2 і b = 3
 """
-
-
-
-if __name__ == "__main__":
-    #TODO
+a=2
+b=3
+def function1(a,b):
+    return (a + 4 * b)*(a - 3 * b) + 2*a
+print(function1(a,b))
